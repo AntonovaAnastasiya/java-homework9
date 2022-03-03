01fbfb9eac0c86b0ef43147bbe958d2e6e814685
PS <89146019257@mail.ru>
2022-03-03 20:01:55 +0900

    Initial commit

---
+++ src/main/java/ru/netology/domain/FormData.java
@@ -0,0 +1,9 @@
+package ru.netology.domain;
+
+public class FormData {
+    public int day;
+    public int month;
+    public int year;
+
+    //+getter+setter;
+}
---
+++ src/main/java/ru/netology/domain/Post.java
@@ -0,0 +1,15 @@
+package ru.netology.domain;
+
+public class Post {
+    public String name; // поле для имени
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public String surname;
+    public boolean subscription;
+
+    public FormData birthday;
+
+    //+getter+setter;
+}
+
